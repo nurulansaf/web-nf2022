fda678814c60f98c4f3d0d59b8ecb270ea4b868c
nurulansaf <nurulansaf@gmail.com>
2022-12-11 16:32:05 +0700

    upload praktikum 10

---
+++ praktikum-10/chat/css/style.css
@@ -0,0 +1,54 @@
+* {
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    height: 2000px;
+    margin: 10px 0;
+}
+
+fieldset {
+    height: 2000px;
+    position: relative;
+    margin: 10px;
+    padding: 30px;
+    box-sizing: border-box;
+}
+
+section {
+    height: 300px;
+    width: 300px;
+    position: fixed;
+    bottom: 50px;
+    right: 50px;
+    margin: 0 auto;
+}
+
+div#textBox {
+    height: 100%;
+    width: 100%;
+    position: relative;
+    padding: 10px;
+    border: 1px solid #000;
+    overflow-x: scroll;
+}
+
+input,
+button {
+    padding: 7px;
+}
+
+.floating {
+    width: 100%;
+    margin: 0 auto;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+}
+
+hr {
+    margin: 3px 0;
+}
